ae6f8fe768350168ea575284038532a3d2f8fc45
nicholasmrice <74073206+nicholasmrice@users.noreply.github.com>
2020-12-05 17:42:22 -0500

    accessed api to add club name, address and logo to page

--- EPL.js
+++ EPL.js
@@ -1,27 +1,22 @@
 $(()=>{
-    ///$('form').on('submit', (event) => {
-       //event.preventDefault();
-      const userInput = $('input[type="text"]').val();
-      console.log(userInput)
-      $.ajax({
-              headers: { 'X-Auth-Token': '66fec67fe350493da1b12ad3cfd9f783' },
-              url: 'https://api.football-data.org/v2/competitions/PL/teams',
-              dataType: 'json',
-              type: 'GET',
-          //}).done(function(response) {
-          // do something with the response, e.g. isolate the id of a linked resource
-          //  console.log(response);
-      //});
-      }).then(
-         (data) => {
-          console.log(data)
+    $.ajax({
+        headers: { 'X-Auth-Token': '66fec67fe350493da1b12ad3cfd9f783' },
+        url: 'https://api.football-data.org/v2/competitions/PL/teams',
+        dataType: 'json',
+        type: 'GET',
+         
+    }).then(
+        (data) => {
+        console.log(data)
 
-          for (let i = 0; i <= (data.teams.length); i++) {
-              //for (let i=0; i <= (data.season.match))
-          $('<li>').text(data.teams[i].name).appendTo('body')
-          $('<li>').text(data.season.currentMatchday).appendTo('body').css('color', "maroon").addClass("matchday");
-          $('<li>').text(data.teams[6].name).appendTo('body').css('color', 'lightblue').addClass("city").attr('id' , 'MCFC');
-      }
+        for (let i = 0; i <= (data.teams.length); i++) {
+            console.log(data.teams[i].crestUrl)
+            const myImage = (data.teams[i].crestUrl)
+
+            $('<li>').text(data.teams[i].name).appendTo('#team-attributes').css('color', "maroon").css('display', 'block').addClass("teams").attr('id', 'teams');
+            $('<li>').text(data.teams[i].address).appendTo('#team-attributes').css('color', "green").css('display', 'block').addClass("address").attr('id', 'address');
+            $('<div>').html(data.teams[i].crestUrl).appendTo('#team-attributes'); 
+        }
       },
           () => {
           console.log('bad');
